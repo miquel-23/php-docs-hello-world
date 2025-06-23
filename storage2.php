5b471d0625cde8fdcdc5b880a88066232007c5b6
miquel-23 <55203340+miquel-23@users.noreply.github.com>
2025-06-23 18:21:43 +0200

    Update storage2.php

--- storage2.php
+++ storage2.php
@@ -5,6 +5,30 @@ use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 
+// Nou
+
+use MicrosoftAzure\Storage\Blob\Models\CreateBlobOptions;
+use MicrosoftAzure\Storage\Blob\Models\BlobSasPermissions;
+use MicrosoftAzure\Storage\Common\Models\SharedAccessSignatureHelper;
+
+$sasHelper = new SharedAccessSignatureHelper(
+    getenv('AZURE_STORAGE_ACCOUNT'),
+    getenv('AZURE_STORAGE_KEY')
+);
+
+$blobName = $blob->getName();
+$sasToken = $sasHelper->generateBlobServiceSharedAccessSignatureToken(
+    'b',             // Resource type (blob)
+    $containerName . '/' . $blobName,
+    'r',             // Permissions: read
+    (new \DateTime())->format('Y-m-d\TH:i:s\Z'),
+    (new \DateTime('+1 hour'))->format('Y-m-d\TH:i:s\Z')
+);
+
+$url = $blob->getUrl() . '?' . $sasToken;
+
+// Nou
+
 // Configuración
 $connectionString = getenv("AZURE_STORAGE_CONNECTION_STRING");
 $containerName = "comprimidos";  // Cambia esto por el nombre de tu contenedor
@@ -60,7 +84,7 @@ try {
     <ul>
         <?php foreach ($blobs as $blob): ?>
             <li>
-                <a href="<?= htmlspecialchars($blob->getUrl()) ?>" target="_blank">
+                <a href="<?= htmlspecialchars($blob->getUrl() . '?' . $sasToken) ?>" target="_blank">
                     <?= htmlspecialchars($blob->getName()) ?>
                 </a>
                 [<a href="?delete=<?= urlencode($blob->getName()) ?>" onclick="return confirm('¿Eliminar este archivo?')">Eliminar</a>]
